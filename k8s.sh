504acb6e4cf3234f9b115ebe5ea0e45f66e765cd
落 <penghai_use@foxmail.com>
2021-07-23 08:45:27 +0800

    2021年 07月 23日 星期五 08:45:27 CST

--- k8s.sh
+++ k8s.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 version=v1.20.8
+arch=amd64
 
 for i in {kube-apiserver,kube-controller-manager,kube-scheduler,kube-proxy}
 do
     mkdir -p $i
-    echo "FROM asia.gcr.io/google-containers/$i:$version" > ./$i/Dockerfile
+    echo "FROM k8s.gcr.io/${i}-${arch}:${version}" > ./$i/Dockerfile
 done
