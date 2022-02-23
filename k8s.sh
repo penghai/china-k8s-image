e437c83c5098199b4d2a49ea91243cf553482cf7
彭海 <penghai@dameng.com>
2022-02-23 17:30:57 +0800

    download k8s-1.22.6

--- k8s.sh
+++ k8s.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-version=v1.20.9
-arch=amd64
+version=v1.22.6
+#arch=amd64
 
 for i in {kube-apiserver,kube-controller-manager,kube-scheduler,kube-proxy}
 do
     mkdir -p $i
-    echo "FROM k8s.gcr.io/${i}-${arch}:${version}" > ./$i/Dockerfile
+    #echo "FROM k8s.gcr.io/${i}-${arch}:${version}" > ./$i/Dockerfile
+    echo "FROM k8s.gcr.io/${i}:${version}" > ./$i/Dockerfile
 done
 
 #k8s.gcr.io/pause:3.2
 #k8s.gcr.io/etcd:3.4.13-0
 #k8s.gcr.io/coredns:1.7.0
 
-for i in {pause:3.2,etcd:3.4.13-0,coredns:1.7.0}
+for i in {pause:3.3,etcd:3.5.0-0,coredns:v1.8.0}
 do
     image_name=$(echo $i|awk -F : '{print $1}')
     mkdir -p $image_name
